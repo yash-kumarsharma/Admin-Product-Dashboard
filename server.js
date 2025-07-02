105d6068517ba3cb11471f68f35c964c4e5402e0
Yash Kumar Sharma <yashkumarsharma1820@gmail.com>
2025-07-02 20:28:38 +0530

    updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const authRoutes = require("./routes/authRoutes");
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
